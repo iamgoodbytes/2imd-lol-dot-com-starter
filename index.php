c2a95d4be4ae76d8be0b53e8a7cdb643aa3a0618
Joris Hens <joris@goodbytes.be>
2023-02-13 13:51:15 +0100

    feat: add initial page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title></title>
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li>
+          <a href="index.php"><img src="lol.png" /></a>
+        </li>
+      </ul>
+    </nav>
+
+    <section class="products">
+        <div class="product">
+            <form action="" method="post">
+                <img src="https://picsum.photos/200/200?grayscale" alt="">
+                <h3 class="product__title">Here is a product for you</h3>
+                <h4 class="product__price">21,<sup>59</sup></h4>
+                <input type="submit">
+            </form>
+        </div>
+      </section>
+  </body>
+</html>
